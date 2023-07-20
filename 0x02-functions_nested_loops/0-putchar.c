5025c5da79528af8a21e6fe192397046c21d1ef1
NotRawan47 <rawankomsan09664@gmail.com>
2023-07-19 18:02:17 -0700

    s

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * 
+ * de
+*/
+
+int main(void)
+{
+	char srt[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+	        _putchar(str[ch]);
+		_putchar('\n');
+		return (0);
+}		
